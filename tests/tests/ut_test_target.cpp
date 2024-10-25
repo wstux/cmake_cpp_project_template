01d5e888fcda740b9006250a2d99b815a242cded
Chistyakov Alexander <wstux1@gmail.com>
2024-10-25 23:22:44 +0300

    * tests: #38 Fixed unit test;

--- tests/tests/ut_test_target.cpp
+++ tests/tests/ut_test_target.cpp
@@ -5,16 +5,17 @@
 #include "shared_lib/shared_lib.h"
 #include "static_lib/static_lib.h"
 
-#define CHECK(td, et) \
-    if (td != et) \
-        std::cerr << "Function call result ('" << td << "') " \
-                  << "not equal etalon data ('" << et << "')" \
-                  << std::endl
+#define CHECK(td, et)                                                       \
+    if (td != et) {                                                         \
+        std::cerr << "[FAIL] Function call result ('" << td << "') "        \
+                  << "not equal etalon data ('" << et << "')" << std::endl; \
+    } else                                                                  \
+        std::cout << "[ OK ] Function call '" << td << "'" << std::endl
 
 int main(int /*argc*/, char** /*argv*/)
 {
     CHECK(interface_lib_func<std::string>(), "interface_lib_func");
-    CHECK(shared_lib_func(), "shared_lib_func");
+    CHECK(shared_lib_func(), "shared_lib");
     CHECK(static_lib_func(), "static_lib_func");
     
     return 0;
