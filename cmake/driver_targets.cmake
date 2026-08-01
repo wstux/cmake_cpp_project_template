ff853a21eb98727e41f7180094778cfd84f5d3ad
Chistyakov Alexander <wstux1@gmail.com>
2026-08-01 15:26:18 +0300

    + cmake: Added function checker of linux headers;

--- cmake/driver_targets.cmake
+++ cmake/driver_targets.cmake
@@ -39,6 +39,25 @@ set(_DRIVER_LISTS_KW    COMPILE_DEFINITIONS
 # Functions
 ################################################################################
 
+function(_IsLinuxHeadersInstalled RESULT)
+    set(${RESULT} 0 PARENT_SCOPE)
+    # 1. Get current kernel version with 'uname -r'
+    execute_process(
+        COMMAND uname -r
+        OUTPUT_VARIABLE _kernel_release
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    # 2. Check headers with standard linux path.
+    find_path(KERNEL_HEADERS_DIR
+        NAMES include/linux/kernel.h
+        PATHS "/usr/src/linux-headers-${_kernel_release}"
+        NO_DEFAULT_PATH
+    )
+    if (KERNEL_HEADERS_DIR)
+        set(${RESULT} 1 PARENT_SCOPE)
+    endif()
+endfunction()
+
 function(_LinuxDriverTarget TARGET_NAME)
     set(_module_target  "${TARGET_NAME}.ko")
     set(_module_name    "${TARGET_NAME}")
